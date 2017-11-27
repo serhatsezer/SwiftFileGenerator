39df99078fd7efe49fa129333dbf818c6ff90d21
Serhat Sezer <serhat.szr@gmail.com>
2017-11-27 11:24:12 +0300

    Create swiftfile_generator.py
    Initial commit

---
+++ swiftfile_generator.py
@@ -0,0 +1,17 @@
+def generateClass(name):
+        f = open("%s.swift" % (name), 'w')
+        f.write("class %s {\n" % (name))
+        f.write("func say() {\n")
+        f.write("       print(\"Say\")\n")
+        f.write("}\n")
+        f.write("\n")
+        f.write("func hello() {\n")
+        f.write("       print(\"Hello\")\n")
+        f.write("}\n")
+        f.write("}")
+        f.close()
+
+def generate(amount):
+  for x in range(0, 2500):
+  generateClass("Class%d" % (x))
+                           
